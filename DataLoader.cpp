4da5f753b1f476286d652bbd2c5824d6f724ac90
Bartosz Operacz <bartek.operacz@o2.pl>
2026-03-18 11:45:55 +0100

    feat: loading data

--- DataLoader.cpp
+++ DataLoader.cpp
@@ -6,49 +6,48 @@
 #include <ranges>
 #include <string_view>
 #include <charconv>
-#include <stdexcept>
+#include <print>
 
 using namespace std;
 
 DataLoader::DataLoader(const string &filename)
 {
-    ifstream file(filename);
-
+    // ios::binary pozostawia \r\n które są uzwględniane w rozmiarze pliku
+    ifstream file(filename, ios::binary);
     if (!file.is_open())
     {
         throw runtime_error("Nie można otworzyć pliku z danymi: " + filename);
     }
 
+    // pobranie rozmiaru pliku z systemu
     auto size = filesystem::file_size(filename); 
+    
+    // wczytanie całego pliku do stringa
+    string data(size, '\0');
+    file.read(data.data(), size);
 
-    string line;
-    vector<array<int, 2>> nodePositions;
-    while (getline(file, line))
-    {
-        if (line.empty()) continue;
-        
-        auto [x, y, profit] = line 
-            | views::split(';') 
-            | views::transform([](auto &&part){ return string_view(part); }) 
-            | views::transform([](string_view part)
-            {
-                int val = 0;
-                
-                auto [ptr, ec] = from_chars(part.data(), part.data() + part.size(), val);
-                
-                if (ec == errc::result_out_of_range) {
-                    throw runtime_error("Zbyt duża liczba w danych");
-                } else if (ec == errc::invalid_argument) {
-                    throw runtime_error("Pole nie będące liczbą w danych");
-                }
-                if (ec != errc{}) {
-                    throw runtime_error("Nieznany błąd w danych");
-                }
-                return val; 
-            }) 
-            | ranges::to<array<int, 3>>();
-        
-        nodePositions.push_back({x, y});
-        nodeProfits.push_back(profit);
-    }
+    // parsowanie danych z użyciem ranges
+    auto parsedData = data 
+        | views::split('\n') 
+        | views::transform([](auto &&line){ return string_view(line); }) 
+        | views::filter([](string_view line){ return !(line.empty() || line == "\r"); }) 
+        | views::transform([](string_view line)
+        {
+            // parsowanie linii na liczby
+            auto res = line 
+                | views::split(';') 
+                | views::transform([](auto &&part){ return string_view(part); }) 
+                | views::transform([](string_view part)
+                {
+                    // parsowanie string_view na int
+                    int val = 0;
+                    // from_chars zignoruje nie usunięte wcześniej możliwie występujące znaki \r na końcu
+                    from_chars(part.data(), part.data() + part.size(), val);
+                    return val; 
+                }) 
+                | ranges::to<vector<int>>();
+            println("{}", res);
+            return res;
+        }) 
+        | ranges::to<vector<vector<int>>>();
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include "include/DataLoader.hpp"
+
 int main(int argc, char *argv[])
 {
+    DataLoader data("../data/TSPA.csv");
 
     return 0;
 }
